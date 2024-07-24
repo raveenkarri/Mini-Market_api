eeaa636e27a184c0da0d12f749a7fa9325522e8b
raveenkarri <raveenkarri@gmail.com>
2024-07-24 20:09:31 +0530

    Database changed from localhost to cloud

--- app.js
+++ app.js
@@ -1,26 +1,28 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require('path');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const path = require("path");
+require("dotenv").config();
 
-const areaRoutes = require('./routes/areas');
+const areaRoutes = require("./routes/areas");
 
 const app = express();
 const PORT = 5002;
 
-// Connect to MongoDB
+// Connect to MongoDBkkk
 mongoose.connect(process.env.MONGODB_URI);
 const db = mongoose.connection;
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to Database'));
+db.on("error", (error) => console.error(error));
+db.once("open", () => console.log("Connected to Database"));
 
 // Middleware
 app.use(cors()); // Add CORS support
 app.use(bodyParser.json());
-app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 // Routes
-app.use('/areas', areaRoutes);
+app.use("/areas", areaRoutes);
 
-app.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
+app.listen(PORT, () =>
+  console.log(`Server running on http://localhost:${PORT}`)
+);
